d20dad138ec3e5a11702e7585a44713601af5046
minmyatthu <minmyatthu2018.mlm@gmail.com>
2021-07-04 22:27:03 +0630

    exercise 3.9

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.use(
