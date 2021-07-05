21e50e47010d995a879b07024c396dd58773a1a9
minmyatthu <minmyatthu2018.mlm@gmail.com>
2021-07-06 00:50:11 +0630

    exercise 3.22

--- phonebook/models/person.js
+++ phonebook/models/person.js
@@ -1,40 +1,38 @@
-const mongoose = require('mongoose');
-var uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGO_URL;
+const url = process.env.MONGO_URL
 
-console.log(`Connecting to ${url}`);
+console.log(`Connecting to ${url}`)
 
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
-});
+})
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     unique: true,
-    minLength: 3,
     required: true,
   },
   number: {
     type: String,
     unique: true,
-    minLength: 8,
     required: true,
   },
-});
+})
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
--- phonebook/mongo.js
+++ phonebook/mongo.js
@@ -1,48 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
     'Please provide the password as an argument: node mongo.js <password>'
-  );
-  process.exit(1);
+  )
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://minmyat:${password}@cluster0.ejlzo.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+const url = `mongodb+srv://minmyat:${password}@cluster0.ejlzo.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
-});
+})
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 5) {
   const person = new Person({
     name: process.argv[3],
     number: process.argv[4],
-  });
+  })
 
   person.save().then((result) => {
-    console.log(
-      `add ${process.argv[3]} number ${process.argv[4]} to phonebook`
-    );
-    mongoose.connection.close();
-  });
+    console.log(`add ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
 if (process.argv.length === 3) {
   Person.find({}).then((result) => {
-    console.log('phonebook: ');
-    result.forEach((person) => console.log(`${person.name} ${person.number}`));
-    mongoose.connection.close();
-  });
+    console.log('phonebook: ')
+    result.forEach((person) => console.log(`${person.name} ${person.number}`))
+    mongoose.connection.close()
+  })
 }
